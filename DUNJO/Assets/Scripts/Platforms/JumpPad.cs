eff3185aee276bac71c9ca56099e67a5942c760a
Cloud <michalcloud2003@gmail.com>
2023-11-19 15:31:56 +0000

    Added a tile map/jumppads/and changed camera settings
    camera is now perspective so we can render stuff better make it look closer further ect. WE NEED MAJOR GRAPHIC OVERHAUL

---
+++ DUNJO/Assets/Scripts/Platforms/JumpPad.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    [SerializeField ]private float bounce = 30f;
+    //AudioManager AudioManager;
+
+    // private void Awake()
+    // {
+    //     //AudioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    // }
+
+    private void OnCollisionEnter2D(Collision2D collision)//check if something collided with object
+    {
+        if(collision.gameObject.CompareTag("Player"))//object collided set to as player
+        {
+            //AudioManager.PlaySFX(AudioManager.JumpPad);
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);//direction multiplied by bounce value
+        }
+    }
+}
