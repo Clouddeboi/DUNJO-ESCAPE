1841b81ba90321de4e8c337f0d0f746c750c9b5f
Cloud <michalcloud2003@gmail.com>
2023-12-07 17:01:33 +0000

    updated

---
+++ DUNJO/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Learned how to make main menu here:https://www.youtube.com/watch?v=DX7HyN7oJjE&t=269s 
+    //And also learned how to make the buttons from flappy bird game which me worked on in class
+    public void PlayGame()
+    {
+        //AudioManager.PlaySFX(AudioManager.StartButtonClick);
+        SceneManager.LoadSceneAsync(1);//loads next scene
+    }
+
+    public void QuitGame()
+    {
+        //AudioManager.PlaySFX(AudioManager.ButtonClick);
+        Application.Quit();//Closes application
+    }
+}
